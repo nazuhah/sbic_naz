3c4c3f190022fa4369a6ed5a5c57622f2964da6c
nazuha <nh00098@surrey.ac.uk>
2011-12-23 21:58:58 +0000

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SbicNaz::Application.config.secret_token = '899ed32bd22544f81a2b9d229315e778ae6249d76d02f7693b17d822d8d70e078298228e4a8cd42142dcdca9307dae8e29fff85a25bd397ad37d12209b4eee95'
